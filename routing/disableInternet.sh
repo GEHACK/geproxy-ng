f4b6895e0143f21bbd42d1121f621598db2bdae6
Mart Pluijmaekers <mart@tuupke.nl>
2022-09-29 00:45:57 +0200

    Adds pxe boot which serves any ubuntu (tested with 2204) image in pxe/html/ubuntu.iso

--- routing/disableInternet.sh
+++ routing/disableInternet.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo 0 > /proc/sys/net/ipv4/ip_forward
+## TODO this needs to be replaced with
+#    echo 'net.ipv4.ip_forward=0' >> /etc/sysctl.conf && sysctl -p
+#    but an idempotent variation
+
+#echo 0 > /proc/sys/net/ipv4/ip_forward
--- routing/enableInternet.sh
+++ routing/enableInternet.sh
@@ -5,21 +5,43 @@ set -a
 source ../.env
 set +a
 
-if [[ ! $(iptables -t nat -C POSTROUTING -o $PUBLIC_INTERFACE -j MASQUERADE 1>/dev/null 2>&1) ]]; then
-  #iptables -t nat -A POSTROUTING -o $PUBLIC_INTERFACE -j MASQUERADE
-  echo 'Add masquarade'
+if [[ -z "$PRIVATE_INTERFACE" ]] || [[ -z "$PUBLIC_INTERFACE" ]]; then
+  echo 'Either $PUBLIC_INTERFACE or $PRIVATE_INTERFACE has not been set in the environment'
+  exit 1
 fi
 
-if [[ ! $(iptables FORWARD -i $PUBLIC_INTERFACE -o $PUBLIC_INTERFACE -m state --state RELATED,ESTABLISHED -j ACCEPT 1>/dev/null 2>&1) ]]; then
-  #iptables -A FORWARD -i $PUBLIC_INTERFACE -o $PUBLIC_INTERFACE -m state --state RELATED,ESTABLISHED -j ACCEPT
+echo 'Attempting to enable forwarding'
+
+iptables -C FORWARD -i $PRIVATE_INTERFACE -o $PUBLIC_INTERFACE -j ACCEPT 1>/dev/null 2>&1
+if [[ ! $? ]]; then
   echo 'Add forward to public'
+  iptables -A FORWARD -i $PRIVATE_INTERFACE -o $PUBLIC_INTERFACE -j ACCEPT
+else
+  echo "To public ($PRIVATE_INTERFACE -> $PUBLIC_INTERFACE) forwarding already setup"
 fi
 
-if [[ ! $(iptables -C FORWARD -i $PRIVATE_INTERFACE -o $PUBLIC_INTERFACE -j ACCEPT 1>/dev/null 2>&1) ]]; then
-  #iptables -A FORWARD -i $PRIVATE_INTERFACE -o $PUBLIC_INTERFACE -j ACCEPT
+
+iptables -C FORWARD -i  $PUBLIC_INTERFACE -o $PRIVATE_INTERFACE -m state --state RELATED,ESTABLISHED -j ACCEPT 1>/dev/null 2>&1
+if [[ ! $? ]]; then
   echo 'Add forward to private'
+  iptables -A FORWARD -i  $PUBLIC_INTERFACE -o $PRIVATE_INTERFACE -m state --state RELATED,ESTABLISHED -j ACCEPT
+else
+  echo "To private ($PUBLIC_INTERFACE -> $PRIVATE_INTERFACE) forwarding already setup"
 fi
 
+iptables -t nat -C POSTROUTING -o $PUBLIC_INTERFACE -j MASQUERADE 1>/dev/null 2>&1
+if [[ ! $? ]]; then
+  echo 'Add masquarade'
+  iptables -t nat -A POSTROUTING -o $PUBLIC_INTERFACE -j MASQUERADE
+else
+  echo "Masquarading (NAT) forwarding already setup"
+fi
+
+
+
+## TODO this needs to be replaced with
+#    echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf && sysctl -p
+#    but an idempotent variation
 
 #echo 1 > /proc/sys/net/ipv4/ip_forward
 
