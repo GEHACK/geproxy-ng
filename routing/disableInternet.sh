0754d62325f3dabc8c19ff42c41d81bee5ea20a4
Mart Pluijmaekers <mart@tuupke.nl>
2022-10-17 12:18:01 +0200

    Adds pixie and better nat enable/disable

--- routing/disableInternet.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## TODO this needs to be replaced with
-#    echo 'net.ipv4.ip_forward=0' >> /etc/sysctl.conf && sysctl -p
-#    but an idempotent variation
-
-#echo 0 > /proc/sys/net/ipv4/ip_forward
