8cea2f6f1c5d4fc705dde2fa7a39fa99f3b22ff8
Mart Pluijmaekers <mart@tuupke.nl>
2024-01-27 23:12:17 +0100

    Pushing so it does not get lost, needs cleaning and rewritten commit

--- nginx/configure.sh
+++ nginx/configure.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-# Remove default
-
 # Enable default listen, if needed this gets overridden with ssl config
 cat <<EOT >/etc/nginx/listen
 listen 80;
 listen [::]:80;
 EOT
 
-if [[ ! -f "$SSL_CERT_LOC" ]] | [[ ! -f "$SSL_KEY_LOC" ]]; then
-  >&2 echo "Either \$SSL_CERT_LOC='$SSL_CERT_LOC', or \$SSL_KEY_LOC='$SSL_KEY_LOC' does not exist. Falling back to HTTP!"
+if [[ ! -f "/etc/nginx/ssl/${DOMAIN}.key" ]] | [[ ! -f "/etc/nginx/ssl/${DOMAIN}.crt" ]]; then
+  >&2 echo "Either '${DOMAIN}.key', or '${DOMAIN}.crt' does not exist. Falling back to HTTP!"
   exit
 fi
 
-echo "Certificate found, attempting to configure SSL for $DOMAIN."
+echo "Certificate found, attempting to configure SSL for ${DOMAIN}."
 
 days=${SSL_CERT_MINIMUM_DAYS:-7}
 secs=$(($days * 24 * 60 * 60))
 
 # Check whether the certificate will expire in a week 7*24*60*60 = 604800
-openssl x509 -noout -dates -checkend $secs <"$SSL_CERT_LOC" 1>/dev/null 2>&1
+openssl x509 -noout -dates -checkend $secs <"/etc/nginx/ssl/$DOMAIN.crt" 1>/dev/null 2>&1
 
 if [[ $? -ne 0 ]]; then
   >&2 echo "Certificate expires within ${days} days (${secs} seconds)! Falling back to HTTP!"
@@ -30,13 +28,13 @@ echo "Certificate is valid for at least ${days} days".
 
 # Check whether the key matches
 # Check if EC cert, otherwise check for RSA
-grep 'EC PRIVATE' gehack.nl.key 1>/dev/null 2>&1
+grep 'EC PRIVATE' "/etc/nginx/ssl/${DOMAIN}.key" 1>/dev/null 2>&1
 if [[ $? ]]; then
-  openssl x509 -in "$SSL_CERT_LOC" -noout -pubkey >/tmp/crt.pubkey 2>/dev/null
-  openssl ec -in "$SSL_KEY_LOC" -pubout >/tmp/key.pubkey 2>/dev/null
+  openssl x509 -in "/etc/nginx/ssl/${DOMAIN}.crt" -noout -pubkey >/tmp/crt.pubkey 2>/dev/null
+  openssl ec -in "/etc/nginx/ssl/${DOMAIN}.key" -pubout >/tmp/key.pubkey 2>/dev/null
 else
-  openssl x509 -noout -modulus -in "$SSL_CERT_LOC" | openssl md5 >/tmp/crt.pubkey
-  openssl rsa -noout -modulus -in "$SSL_KEY_LOC" | openssl md5 >/tmp/key.pubkey
+  openssl x509 -noout -modulus -in "/etc/nginx/ssl/${DOMAIN}.crt" | openssl md5 >/tmp/crt.pubkey
+  openssl rsa -noout -modulus -in "/etc/nginx/ssl/${DOMAIN}.key" | openssl md5 >/tmp/key.pubkey
 fi
 
 # Check if there is a match
@@ -47,19 +45,19 @@ if [[ ! $? ]]; then
 fi
 
 # Check for subdomains
-names=$(openssl x509 -noout -ext subjectAltName <"$SSL_CERT_LOC" | grep -o -e "\([\*A-Za-z0-9_-]\.\)*$DOMAIN")
-if [[ "${names}" == *"*.$DOMAIN"* ]]; then
+names=$(openssl x509 -noout -ext subjectAltName <"/etc/nginx/ssl/${DOMAIN}.crt" | grep -o -e "\([\*A-Za-z0-9_-]\.\)*${DOMAIN}")
+if [[ "${names}" == *"*.${DOMAIN}"* ]]; then
   echo "Certificate contains wildcard, awesome!"
 else
   # Check whether all subdomains are contained in the names
   readarray -td, subs <<<"$SUBDOMAINS,"
   unset 'subs[-1]'
   for f in "${subs[@]}"; do
-    if [[ ! "${names}" == *"${f}.$DOMAIN"* ]]; then
-      >&2 echo "Subdomain '${f}.$DOMAIN' not in certificate '$SSL_CERT_LOC'! Falling back to HTTP!"
+    if [[ ! "${names}" == *"${f}.${DOMAIN}"* ]]; then
+      >&2 echo "Subdomain '${f}.${DOMAIN}' not in certificate '/etc/nginx/ssl/${DOMAIN}.crt'! Falling back to HTTP!"
       exit
     else
-      echo "Subdomain '${f}.$DOMAIN' found in certificate '$SSL_CERT_LOC'."
+      echo "Subdomain '${f}.${DOMAIN}' found in certificate '/etc/nginx/ssl/${DOMAIN}.crt'."
     fi
   done
 fi
@@ -68,21 +66,22 @@ fi
 echo "Setting up SSL!"
 
 # Add a http->https redirect
-cat <<EOT >/etc/nginx/conf.d/redirect
+cat <<"EOT" >/etc/nginx/conf.d/redirect
 server {
         listen   80 default_server;
         server_name _default_;
-        return 308 https://\$host\$request_uri;  # enforce https
+        return 308 https://$host$request_uri;  # enforce https
 }
 EOT
 
 # Override the listen helper, note this is a basic ssl config and needs to be improved.
 cat <<EOT >/etc/nginx/listen
-listen   443 ssl http2;
-listen   [::]:443 ssl http2;
+listen   443 ssl;
+listen   [::]:443 ssl;
+http2    on;
 
-ssl_certificate $SSL_CERT_LOC;
-ssl_certificate_key $SSL_KEY_LOC;
+ssl_certificate /etc/nginx/ssl/${DOMAIN}.crt;
+ssl_certificate_key /etc/nginx/ssl/${DOMAIN}.key;
 
 ssl_session_timeout 5m;
 ssl_prefer_server_ciphers on;
